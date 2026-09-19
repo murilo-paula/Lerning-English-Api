f958dd202ed01f82c1363b34233ddf3a34f37ca4
murilo-paula <murilo.pauladsz@gmail.com>
2026-09-19 20:26:14 -0300

    feat: connect prisma with mysql

--- src/prisma/prisma.module.ts
+++ src/prisma/prisma.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
 
 @Module({
+  imports: [PrismaService],
   providers: [PrismaService]
 })
 export class PrismaModule {}
--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,4 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '../../generated/prisma/client';
 
 @Injectable()
-export class PrismaService {}
+export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+    constructor() {
+        const adapter = new 
+    }
+}
