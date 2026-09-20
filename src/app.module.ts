6648c782375261b74459d952cd5195ab9a0b06fc
murilo-paula <murilo.pauladsz@gmail.com>
2026-09-20 09:16:54 -0300

    feat: findEmail

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { createObserveModule } from '@nestjs/observe';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { UsersModule } from './users/users.module';
 
 export const { ObserveModule, ObserveInstrument } = createObserveModule();
 
@@ -16,6 +17,7 @@ export const { ObserveModule, ObserveInstrument } = createObserveModule();
       serviceId: 'lerning-english-project-api',
     }),
     PrismaModule,
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ src/users/users.service.ts
@@ -0,0 +1,39 @@
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { PrismaService } from '../prisma/prisma.service';
+
+@Injectable()
+export class UsersService {
+  constructor(private prisma: PrismaService) {}
+
+  create(bodyUser: CreateUserDto) {
+    const userIsExisting = 
+
+    return ;
+  }
+
+  async findEmail(email: string) {
+    try {
+      return await this.prisma.users.findUnique( { where: {email: email} } )
+    } catch (error) {
+      throw new InternalServerErrorException("findEmail error")
+    }
+  }
+
+  findAll() {
+    return `This action returns all users`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} user`;
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return `This action updates a #${id} user`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
+}
